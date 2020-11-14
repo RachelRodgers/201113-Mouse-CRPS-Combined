c65da5a222f0b9a4128ab49bf042025256bcc272
Rachel Rodgers <rachel.rodgers@wustl.edu>
2020-11-14 11:20:27 -0600

    initial commit

---
+++ 201113_Mouse_CRPS_Combined_Format_Metadata.R
@@ -0,0 +1,57 @@
+# 201113_Mouse_CRPS_Combined_Format_Metadata.R
+
+# Combining metadata for mouse CRPS cohort 1 and cohort 2.
+# Needed variables:
+#   sample, cage, week, condition ("control", "fracture"), num_reads,
+#   crps_type("control", "acute", "chronic")   
+
+library("tidyverse")
+
+dataCohort1 <- readRDS("../data/metadata/sampleData_crps_cohort1.RDS")
+dataCohort2 <- readRDS("../data/metadata/sampleData_crps_cohort2.RDS")
+
+#----- Format cohort 1 -----#
+
+weekNum <- c("0", "3", "5", "9", "16")
+weekRenameLUT <- paste0("week_", weekNum)
+names(weekRenameLUT) <- weekNum
+
+formatCohort1 <- dataCohort1 %>% 
+  select(SampleName, Cage, Week, Treatment, ReadsPerSample) %>% 
+  rename(sample = SampleName, cage = Cage, week = Week, condition = Treatment,
+         num_reads = ReadsPerSample) %>% 
+  mutate(week = weekRenameLUT[week],
+         cohort = "cohort_1") %>% 
+  filter(condition != "none")
+
+#----- Format cohort 2 -----#
+
+# read in cage data for cohort 2 (received Nov 14, 2020)
+cageCohort2 <- read.delim("../data/cage_201019_LaraC_V4_Mouse_CRPS.txt",
+                          stringsAsFactors = FALSE)
+
+cageLUT <- cageCohort2 %>% 
+  select(mouse_number, cage) %>% 
+  deframe()
+
+formatCohort2 <- dataCohort2 %>% 
+  mutate(cage = cageLUT[mouse_number]) %>% 
+  select(sample, cage, week, group, ReadsPerSample) %>% 
+  rename(condition = group, num_reads = ReadsPerSample) %>% 
+  mutate(condition = ifelse(condition == "treatment",
+                            yes = "fracture", no = "control"),
+         cohort = "cohort_2")
+
+#----- Combine -----#
+# Combine, add crps type.
+
+combinedMetadata <- rbind(formatCohort1, formatCohort2) %>% 
+  mutate(crps_type = case_when(week %in% c("week_3", "week_5") ~ "acute",
+                               week %in% c("week_9", "week_16") ~ "chronic",
+                               TRUE ~ "control"))
+
+row.names(combinedMetadata) <- combinedMetadata$sample
+
+write.table(combinedMetadata, 
+            file = "../data/metadata/201113_Mouse_CRPS_combinedMetadata.txt",
+            quote = FALSE, sep = "\t")
---
+++ README.md
@@ -0,0 +1,9 @@
+# 201113-Mouse-CRPS-Combined
+
+Combined 16S analysis of first mouse cohort (190809_Lara_16S_Mouse_CRPS) and second mouse cohort (201019_LaraC_V4_Mouse_CRPS).
+
+Analysis includes:
+	
+	1. Four group comparison: control 1, control 2, treatment 1, treatment 2
+	
+	2. Two group comparison: combined controls, combined treatments
